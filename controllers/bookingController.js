c3a677e4769a0c0339495e0a62eb02b6da7b3a4f
Raviphas-R <iamnice2020@gmail.com>
2023-03-29 10:56:59 +0700

    modified:   controllers/bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,6 @@
 const Stripe = require('stripe');
 const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
-const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const User = require('../models/userModel');
@@ -60,7 +59,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = seesion.line_items[0].unit_amount / 100;
+  const price = session.line_items[0].unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
