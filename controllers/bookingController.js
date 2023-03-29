22514cbeb24a13780e59327fb656c11338c669c9
Raviphas-R <iamnice2020@gmail.com>
2023-03-29 11:21:24 +0700

    fix bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,12 +54,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   await Booking.create({ tour, user, price });
 
 //   res.redirect(req.originalUrl.split('?')[0]);
-// });
+// });W
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount_total / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
