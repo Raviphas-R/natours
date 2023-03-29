195f86df0d8e73960b341c287dc489e7d6046952
Raviphas-R <iamnice2020@gmail.com>
2023-03-29 13:36:26 +0700

    add alert booking

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -4,7 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(viewController.alerts);
+
 router.get('/', authController.isLoggedIn, viewController.getOverview);
+
 router.get('/tours/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLogin);
 router.get('/me', authController.protect, viewController.getAccount);
